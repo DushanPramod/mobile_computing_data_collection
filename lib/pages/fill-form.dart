7c23df38beee7532e26e331d31524bbbe7ff5dcb
Dushan Pramod <dushanpramod@gmail.com>
2023-02-21 14:30:43 +0530

    file form page updated

--- lib/pages/fill-form.dart
+++ lib/pages/fill-form.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 import 'package:intl/intl.dart';
+import 'package:chewie/chewie.dart';
 
 class FillForm extends StatefulWidget {
   final String formId;
@@ -21,6 +26,8 @@ class _FillFormState extends State<FillForm> {
     InputTypeMdl(1, 'Age', 'number', ''),
     InputTypeMdl(2, 'Birthday', 'date', ''),
     InputTypeMdl(3, 'Time', 'time', ''),
+    InputTypeMdl(4, 'Image', 'image', ''),
+    InputTypeMdl(5, 'Video', 'video', ''),
   ];
 
   Widget _buildTextInput(InputTypeMdl data) {
@@ -88,6 +95,8 @@ class _FillFormState extends State<FillForm> {
               ? DateFormat('yyyy-MM-dd').format(DateTime.parse(data.data))
               : '',
           decoration: InputDecoration(
+            enabled: true,
+              helperText: 'yyyy-mm-dd',
               labelText: data.fieldName,
               suffixIcon: Container(
                 margin: const EdgeInsets.all(5),
@@ -124,7 +133,6 @@ class _FillFormState extends State<FillForm> {
     ));
   }
 
-
   Widget _buildTimeInput(InputTypeMdl data) {
     return Center(
         child: Column(
@@ -133,6 +141,8 @@ class _FillFormState extends State<FillForm> {
             TextFormField(
               initialValue: data.data,
               decoration: InputDecoration(
+                  enabled: true,
+                  helperText: 'HH:mm',
                   labelText: data.fieldName,
                   suffixIcon: Container(
                     margin: const EdgeInsets.all(5),
@@ -168,6 +178,119 @@ class _FillFormState extends State<FillForm> {
         ));
   }
 
+  XFile? imageFile;
+  XFile? videoFile;
+  void _openGallery(BuildContext context, String dataType) async {
+    if(dataType == 'image'){
+      var picture = await ImagePicker().pickImage(source: ImageSource.gallery);
+      setState(() {
+        imageFile = picture;
+      });
+    }
+    else{
+      var video = await ImagePicker().pickVideo(source: ImageSource.gallery);
+      setState(() {
+        videoFile = video;
+      });
+    }
+
+    if (context.mounted) Navigator.of(context).pop();
+    // Navigator.of(context).pop();
+  }
+
+  void _openCamera(BuildContext context, String dataType) async {
+    if(dataType == 'image'){
+      var picture = await ImagePicker().pickImage(source: ImageSource.camera);
+      setState(() {
+        imageFile = picture;
+      });
+    }
+    else{
+      var video = await ImagePicker().pickVideo(source: ImageSource.camera);
+      setState(() {
+        videoFile = video;
+      });
+    }
+    if (context.mounted) Navigator.of(context).pop();
+    // Navigator.of(context).pop();
+  }
+
+  Widget _buildImageInput(InputTypeMdl data) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(data.fieldName, style: const TextStyle(fontSize: 16,fontWeight: FontWeight.w500)),
+        imageFile == null?const Text('No Image Selected'): Image.file(File(imageFile!.path), width: 200, height: 200),
+        // const Text('No image selected'),
+        ElevatedButton(onPressed: () {
+          _showChoiceDialog(context, data);
+        }, child: const Text('Select Image'))
+      ],
+    );
+  }
+
+  Widget _buildVideoInput(InputTypeMdl data) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(data.fieldName, style: const TextStyle(fontSize: 16,fontWeight: FontWeight.w500)),
+            ElevatedButton(onPressed: () {
+              _showChoiceDialog(context, data);
+            }, child: const Text('Select Video'))
+          ],
+        ),
+        Container(
+          color: Colors.brown,
+          height: MediaQuery.of(context).size.height * (30/100),
+          width: MediaQuery.of(context).size.width *  (100/100),
+          child: videoFile == null?const Center(
+            child: Icon(Icons.videocam, color: Colors.red, size: 50.0),
+          ):FittedBox(
+            fit: BoxFit.contain,
+            child: mounted?Chewie(
+              controller: ChewieController(
+                  videoPlayerController: VideoPlayerController.file(File(videoFile!.path)),
+                  aspectRatio: 3/2,
+                  autoPlay: false,
+                  looping: false
+              ),
+            ):Container(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _showChoiceDialog(context, InputTypeMdl data) {
+    return showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: const Text('Choose'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              GestureDetector(
+                child: const Text('Gallery'),
+                onTap: (){
+                  _openGallery(context,data.dataType);
+                },
+              ),
+              const Padding(padding: EdgeInsets.all(8.0)),
+              GestureDetector(
+                child: const Text('Camera'),
+                onTap: (){
+                  _openCamera(context,data.dataType);
+                },
+              )
+            ],
+          ),
+        ),
+      );
+    });
+  }
 
 
   @override
@@ -200,6 +323,10 @@ class _FillFormState extends State<FillForm> {
                         return _buildDateInput(e);
                       } else if (e.dataType == 'time') {
                         return _buildTimeInput(e);
+                      } else if (e.dataType == 'image') {
+                        return _buildImageInput(e);
+                      } else if (e.dataType == 'video') {
+                        return _buildVideoInput(e);
                       } else {
                         return _buildTextInput(e);
                       }
@@ -226,7 +353,7 @@ class InputTypeMdl {
   int inputNo;
   String fieldName;
   String dataType;
-  String data;
+  dynamic data;
 
   InputTypeMdl(this.inputNo, this.fieldName, this.dataType, this.data);
 }
