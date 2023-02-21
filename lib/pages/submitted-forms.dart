fd2d1d7451ad682581145680f83399c5245a6e69
Thushan97 <63712032+Thushan97@users.noreply.github.com>
2023-02-21 14:29:49 +0530

    feat/get form data completed

--- lib/pages/submitted-forms.dart
+++ lib/pages/submitted-forms.dart
@@ -1,58 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:mobile_computing_data_collection/models/submitted-forms.mdl.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class MyCustomObject {
+  final String title;
+  late DateTime updatedDate;
+
+  MyCustomObject({required this.title, required this.updatedDate});
+}
 
 class SubmittedForms extends StatefulWidget {
   const SubmittedForms({Key? key}) : super(key: key);
+
 
   @override
   State<SubmittedForms> createState() => _SubmittedFormsState();
 }
 
 class _SubmittedFormsState extends State<SubmittedForms> {
-  List<SubmittedFormModel> submittedForms = [
-    SubmittedFormModel(
-        id: '1',
-        title: 'test 1',
-        researcher: 'name 1',
-        submittedDate: DateTime.parse('2023-02-18 13:00:04Z')),
-    SubmittedFormModel(
-        id: '1',
-        title: 'test 2',
-        researcher: 'name 1',
-        submittedDate: DateTime.parse('2023-02-18 13:01:04Z')),
-    SubmittedFormModel(
-        id: '1',
-        title: 'test 3',
-        researcher: 'name 1',
-        submittedDate: DateTime.parse('2023-02-18 13:02:04Z')),
-    SubmittedFormModel(
-        id: '1',
-        title: 'test 4',
-        researcher: 'name 1',
-        submittedDate: DateTime.parse('2023-02-18 13:03:04Z'))
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return ListView(
-        padding: const EdgeInsets.all(8),
-        children: submittedForms.map((e) {
-          return Card(
-              child: ListTile(
-            onTap: () {},
-            title: Text(e.title),
-            subtitle: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(e.researcher),
-                Text(DateFormat('yyyy-MM-dd-kk:mm').format(e.submittedDate)),
-              ],
-            ),
-            leading: const Icon(Icons.upload_file, size: 50),
-            // trailing: Text(e.numOfRecords)
-          ));
-        }).toList());
+    CollectionReference collection = FirebaseFirestore.instance.collection('submitted_form_list');
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: collection.where('userId', isEqualTo: userId).snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        List<MyCustomObject> myObjects = snapshot.data!.docs.map((DocumentSnapshot document) {
+          return MyCustomObject(
+              title: document.get('title'),
+              updatedDate: document.get('updatedDate')?.toDate()
+          );
+        }).toList();
+
+        return Scaffold(
+          body: ListView.builder(
+            itemCount: myObjects.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  onTap: () {},
+                  title: Text(myObjects[index].title),
+                  subtitle: Text(DateFormat('yyyy-MM-dd-kk:mm').format(myObjects[index].updatedDate)),
+                  leading: const Icon(Icons.upload_file, size: 50),
+                ),
+              );
+
+            },
+          ),
+
+        );
+      },
+    );
   }
 }
+
+
+
